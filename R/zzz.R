6735d9cc54dd792e26b5509c4388ad8419ed380e
damianosmichaelides <117011863+damianosmichaelides@users.noreply.github.com>
2025-03-20 15:24:46 +0200

    Add files via upload

--- R/zzz.R
+++ R/zzz.R
@@ -4,19 +4,43 @@
 #' It ensures that the showtext package and Noto Sans font are registered properly to support Unicode fonts.
 #' @keywords internal
 #' @noRd
+#' 
+
 
 .onLoad <- function(libname, pkgname) {
-  if (!requireNamespace("showtext", quietly = TRUE)) {
-    warning("showtext package is not installed. Unicode fonts may not render correctly.")
-    return()
-  }
+  font_path <- system.file("fonts", "NotoSansMath-Regular.ttf", package = "hassediagram")
   
-  showtext::showtext_auto()
-  
-  if (!"Noto Sans Math" %in% names(sysfonts::font_families())) {
-    sysfonts::font_add_google("Noto Sans Math", "noto")
+  if (file.exists(font_path)) {
+    sysfonts::font_add("noto", font_path)
+    showtext::showtext_auto()
+  } else {
+    warning("Noto Sans Math font is missing. Please install manually with sysfonts::font_add_google().")
   }
 }
+
+
+# .onLoad <- function(libname, pkgname) {
+#   font_path <- system.file("fonts/NotoSansMath-Regular.ttf", package = "hassediagram")
+#   if (file.exists(font_path)) {
+#     sysfonts::font_add("noto", font_path)
+#   } else {
+#     warning("Noto Sans Math font is missing. Install manually with sysfonts::font_add_google().")
+#   }
+# }
+
+
+# .onLoad <- function(libname, pkgname) {
+#   if (!requireNamespace("showtext", quietly = TRUE)) {
+#     warning("showtext package is not installed. Unicode fonts may not render correctly.")
+#     return()
+#   }
+#   
+#   showtext::showtext_auto()
+#   
+#   if (!"Noto Sans Math" %in% names(sysfonts::font_families())) {
+#     sysfonts::font_add_google("Noto Sans Math", "noto")
+#   }
+# }
 
 
 
